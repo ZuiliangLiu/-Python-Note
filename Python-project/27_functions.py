022c51fce8027957ecd8ab0a0d5e80764f1b39b4
ZuiliangLiu <1553178962@qq.com>
2026-09-18 16:09:44 +0800

    Create 27_functions.py

---
+++ Python-project/27_functions.py
@@ -0,0 +1,15 @@
+def happy_birthday(name,age):
+    print(f"Happy {age}th birthday, {name}!")
+
+
+happy_birthday("Suise"  ,23)
+
+
+def add(x,y):
+    z = x+y
+    return z
+def subtract(x,y):
+    z = x-y
+    return z
+print(add(2,3))
+print(subtract(2,3))
